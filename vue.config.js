2b2924327857f6d1a9bc3e0aa04d8298e958679e
Luan <luan.sievers@gmail.com>
2019-06-23 22:53:19 -0300

    refactor

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: "pt_BR",
-      fallbackLocale: "en_US",
+      locale: "pt-br",
+      fallbackLocale: "en-us",
       localeDir: "locales",
       enableInSFC: false
     }
